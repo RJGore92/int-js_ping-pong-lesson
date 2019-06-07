964371f947bea8046c6b084ed5807eda12906e4d
Robert Gore <DarkburstGX@gmail.com>
2019-06-07 13:44:10 -0700

    initial project commit for ping-pong lesson.  needs bugfixing, but will be running soon.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './styles.css';
+import { pingPong } from './ping-pong';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
